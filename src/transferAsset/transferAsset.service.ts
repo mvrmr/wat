bbb7b7db6bd60a4f1ccef65c501b4697e897eef0
Venkata Ranga Mohan Rao Manchikanti <mohan.manchikanti@consensys.net>
2023-12-07 12:59:08 +0000

    receiverTest implemented as API

--- src/transferAsset/transferAsset.service.ts
+++ src/transferAsset/transferAsset.service.ts
@@ -1,61 +1,30 @@
 import { Injectable } from '@nestjs/common';
-
 import { utils } from 'ffjavascript';
-
-// import { unstringifyBigInts } = require('ffjavascript').utils;
-// import fs = require('fs');
-
 import snarkjs = require('snarkjs');
 import { readFileSync } from 'node:fs';
 import circomlibjs = require('circomlibjs');
-// import commander = require('commander');
-
-import { Command } from 'commander';
-const program = new Command();
 
 @Injectable()
 export class TransferAssetService {
   receiverTest(
     sendAmount: number,
-    receiverstartingbalance: number,
-    receiversalt: number,
+    receiverStartingBalance: number,
+    receiverSalt: number,
   ): any {
-    return this.runReceivetTest(sendAmount, receiverstartingbalance, receiversalt);
-    return `About run 'receiver Test' with Parameters sendAmount, receiverstartingbalance, receiversalt: ${sendAmount}, ${receiverstartingbalance}, ${receiversalt}`;
+    return this.runReceivetTest(
+      sendAmount,
+      receiverStartingBalance,
+      receiverSalt,
+    );
+
+    // return `About run 'receiver Test' with Parameters sendAmount, receiverStartingBalance, receiverSalt: ${sendAmount}, ${receiverStartingBalance}, ${receiverSalt}`;
   }
 
-  runReceivetTest = async function (    sendAmount: number,
-    receiverstartingbalance: number,
-    receiversalt: number,) {
-    // Use commander library to parse command line inputs
-    program
-      .version('1.0.0', '-v, --version')
-      .usage('[OPTIONS]...')
-      .option(
-        '-a, --sendamount <Amount of Tokens to Transfer>',
-        'Overwriting value.',
-        '0',
-      )
-      .option(
-        '-b, --receiverstartingbalance <Staring Balance of Receiver Account>',
-        'Overwriting value.',
-        '0',
-      )
-      .option(
-        '-s,  --receiversalt <Random Salt owned by Receiver>',
-        'Overwriting value.',
-        '0',
-      )
-      .parse(process.argv);
-  
-    const options = program.opts();
-
-    // Define private witness input variables and intermediary signals
-    const receiverSalt = Number(`${options.receiversalt}`);
-    const sendAmount = Number(`${options.sendamount}`);
-    const receiverStartingBalance = Number(
-      `${options.receiverstartingbalance}`,
-    );
+  runReceivetTest = async function (
+    sendAmount: number,
+    receiverStartingBalance: number,
+    receiverSalt: number,
+  ) {
     const receiverEndingBalance = receiverStartingBalance + sendAmount;
 
     // Calculate public Hash values
@@ -64,20 +33,11 @@ export class TransferAssetService {
     const receiverStartingBalanceHash = poseidon.F.toString(
       poseidon([receiverStartingBalance + receiverSalt]),
     );
-    console.log(
-      `receiverStartingBalance='${receiverStartingBalance}'; receiverStartingBalanceHash='${receiverStartingBalanceHash}'`,
-    );
     const receiverEndingBalanceHash = poseidon.F.toString(
       poseidon([receiverEndingBalance + receiverSalt]),
     );
-    console.log(
-      `receiverEndingBalance='${receiverEndingBalance}'; receiverEndingBalanceHash='${receiverEndingBalanceHash}'`,
-    );
     const sendAmountHash = poseidon.F.toString(
       poseidon([sendAmount + receiverSalt]),
-    );
-    console.log(
-      `sendAmount='${sendAmount}'; sendAmountHash='${sendAmountHash}'`,
     );
 
     // Run the circuit, generating the proof
@@ -90,11 +50,21 @@ export class TransferAssetService {
       'build/circom/confidential_transaction_receiver_js/confidential_transaction_receiver.wasm',
       'keys/receiver_proving_key.zkey',
     );
-    console.log(`publicSignals:\n`, publicSignals);
-    console.log(`proof:\n`, proof);
+
+    console.log(
+      `receiverStartingBalance='${receiverStartingBalance}'; receiverStartingBalanceHash='${receiverStartingBalanceHash}'\n`,
+      `receiverEndingBalance='${receiverEndingBalance}'; receiverEndingBalanceHash='${receiverEndingBalanceHash}'\n`,
+      `sendAmount='${sendAmount}'; sendAmountHash='${sendAmountHash}'\n`,
+      `publicSignals:\n`,
+      publicSignals,
+      `proof:\n`,
+      proof,
+    );
 
     // Verify that the proof is valid
-    const vKey = JSON.parse(readFileSync('keys/receiver_verification_key.json').toString());
+    const vKey = JSON.parse(
+      readFileSync('keys/receiver_verification_key.json').toString(),
+    );
 
     const res = await snarkjs.groth16.verify(vKey, publicSignals, proof);
 
@@ -110,10 +80,21 @@ export class TransferAssetService {
         JSON.parse(`[${rawCallData}]`),
       );
       console.log(`jsonCallData:\n`, jsonCallData);
+
+      return {
+        receiverStartingBalance,
+        sendAmount,
+        receiverEndingBalance,
+        receiverStartingBalanceHash,
+        receiverEndingBalanceHash,
+        sendAmountHash,
+        publicSignals,
+        proof,
+        // jsonCallData_Type: typeof jsonCallData,
+        stringJsonCallData: String(jsonCallData),
+      };
     } else {
       console.log('Invalid proof');
     }
-
-    process.exit(0);
   };
 }
