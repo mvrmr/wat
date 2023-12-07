9db2b84c2a61cfb2fbfd75e20658147013189f09
Venkata Ranga Mohan Rao Manchikanti <mohan.manchikanti@consensys.net>
2023-12-07 17:17:20 +0000

    Fix gap with numeric parms in transferAsset.receiverTest-2

--- src/transferAsset/transferAsset.controller.ts
+++ src/transferAsset/transferAsset.controller.ts
@@ -9,9 +9,9 @@ export class TransferAssetController {
 
   @Get('receiverTest/:sendAmount/:receiverstartingbalance/:receiversalt')
   receiverTest(
-    @Param('sendAmount') sendAmount,
-    @Param('receiverstartingbalance') receiverstartingbalance,
-    @Param('receiversalt') receiversalt,
+    @Param('sendAmount') sendAmount: number,
+    @Param('receiverstartingbalance') receiverstartingbalance: number,
+    @Param('receiversalt') receiversalt: number,
   ): string {
     sendAmount =
       typeof sendAmount == 'string' ? parseInt(sendAmount) : sendAmount;
